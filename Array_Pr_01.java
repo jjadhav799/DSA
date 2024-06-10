c8291d91d39745bc6b3d8c753c93e2cee6d70272
JAYESH ABHIMAN JADHAV <jjadhav799@gmail.com>
2024-06-10 22:15:47 +0530

    Create Array_Pr_01.java

---
+++ Array_Pr_01.java
@@ -0,0 +1,19 @@
+// given element of array so rotate the array from the end to start for the k time 
+
+// actual solutoion is line number : - 
+class Array_Pr_01 
+{
+  public static void main () 
+  {
+    int arr[] = { 3,-2,1,4,6,9,8} ; // this is given array 
+    int k = 3 ; 
+
+    // printing the orignal array 
+    for ( int i = 0 ; i < arr.length; i++) 
+      {
+    System.out.println(arr[i]);
+      }// end of the for loop of print 
+    // for rotation we should be the 
+    
+  }
+}
